9b7bf18a91653c679ec12055ba8a177be8a2333e
Léo Cabart <cabartla@gmail.com>
2024-10-29 14:42:50 +0000

    int_orders_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,8 +5,8 @@ SELECT
     revenue,
     quantity,
     purchase_price,
-    CAST(quantity*purchase_price as INT64) as purchase_cost,
-    CAST (revenue-(quantity*purchase_price) as INT64) as margin
+    CAST(quantity*purchase_price as FLOAT6464) as purchase_cost,
+    CAST (revenue-(quantity*purchase_price) as FLOAT64) as margin
 from {{ ref('stg_raw__sales') }}
 LEFT JOIN {{ ref('stg_raw__product') }}
 USING (products_id)
